543fd285b9a88b2242d588940e55ee9ab88b0c74
Robert Bates <thegreatbobini@gmail.com>
2025-07-02 12:33:09 -0700

    updating backgrounds game

--- MLCollage/Views/NewViews/AllBackgroundsView.swift
+++ MLCollage/Views/NewViews/AllBackgroundsView.swift
@@ -13,14 +13,19 @@ struct AllBackgroundsView: View {
     @Query private var backgrounds: [BackgroundModel]
     
     var body: some View {
-        Grid {
-            ForEach(backgrounds) { background in
-                Image(uiImage: background.toMLCImage().uiImage)
-                    .resizable()
-                    .clipShape(.rect(cornerRadius: 10.0))
-                    .frame(maxWidth: 100, maxHeight: 100)
-                    .padding(3.0)
+        ScrollView {
+            LazyVGrid(columns: [
+                .init(.adaptive(minimum: 100, maximum: .infinity), spacing: 3)
+            ]) {
+                ForEach(backgrounds) { background in
+                    Image(uiImage: background.toMLCImage().uiImage)
+                        .resizable()
+                        .clipShape(.rect(cornerRadius: 10.0))
+                        .frame(maxWidth: 100, maxHeight: 100)
+                        .padding(3.0)
+                }
             }
+            .padding(.horizontal)
         }
     }
 }
