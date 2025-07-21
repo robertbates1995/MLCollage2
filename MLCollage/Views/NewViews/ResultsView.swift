3b1c202df508bdbb4e87723bf7005689315c5872
Robert Bates <thegreatbobini@gmail.com>
2025-07-21 14:10:30 -0700

    fixed preview and removed redundant title

--- MLCollage/Views/NewViews/ResultsView.swift
+++ MLCollage/Views/NewViews/ResultsView.swift
@@ -10,23 +10,8 @@ import SwiftUI
 struct ResultsView: View {
     var body: some View {
         VStack {
-            HStack {
-                Text("Results")
-                    .font(.largeTitle)
-                    .padding(.horizontal)
-                Spacer()
-                Button("View All") {
-
-                }
-                .padding(5.0)
-                .background(Color.gray.opacity(0.2))
-                .cornerRadius(10.0)
-                .padding(.horizontal)
-            }
-            .background(.secondaryAccent)
             VStack {
                 //make gridview here
-                    
             }
             .background(Color.gray.opacity(0.2))
             .cornerRadius(10.0)
@@ -38,5 +23,7 @@ struct ResultsView: View {
 }
 
 #Preview {
-    ResultsView()
+    NavigationView {
+        ResultsView()
+    }
 }
